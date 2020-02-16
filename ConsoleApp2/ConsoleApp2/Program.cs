9e8427c7e521de95881c46d810573b3cde31e7f4
kamilcyan <kamil.lucjanek@wp.pl>
2020-02-16 21:07:07 +0100

    dodany przykład łamania zasady single responsibility principale

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Properties properties = new Properties();
+
+            properties.Color = "red";
+            properties.Shape = "triangle";
+
+            properties.objectProperties();
+            Console.WriteLine("Object properties: " + properties.Color + " " + properties.Shape);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Properties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Properties
+    {
+        public string Color { get; set; }
+        public string Shape { get; set; }
+
+        public void objectProperties()
+        {
+            Console.WriteLine("Object shape is " + Shape);
+            Console.WriteLine("Object color is " + Color);
+        }
+    }
+}
