180ab74a3378020f4e3c45555b18e0f9585d5da8
kamilcyan <kamil.lucjanek@wp.pl>
2020-02-16 22:07:11 +0100

    this

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,7 +4,7 @@ namespace ConsoleApp2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             Properties properties = new Properties();
 
